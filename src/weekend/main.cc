68da2090d3d512ddc38236038b9a5a17f7fca45e
octopusdims <octopusdims@gmail.com>
2026-01-12 13:15:35 +0800

    Added progress reporting

--- src/weekend/main.cc
+++ src/weekend/main.cc
@@ -2,9 +2,9 @@
 int main() {
     int image_width = 256;
     int image_height = 256;
-
     std::cout<<"P3\n"<<image_width<<' '<<image_height<<"\n255\n";
     for(int j=0;j<image_width;j++){
+        std::clog<<"\rScanlines remaining:"<<(image_height-j)<<' '<<std::flush;
         for(int i=0;i<image_height;i++){
             auto r=double(i)/(image_width-1);
             auto g=double(j)/(image_height-1);
@@ -17,4 +17,5 @@ int main() {
             std::cout<<ir<<' '<<ig<<' '<<ib<<'\n';
         }
     }
+    std::clog <<"\rDone.                 \n";
 }
