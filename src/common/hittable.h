213545c11a4600304e8bb6802b1353942c0b1757
zhanghanyu <octopusdims@gmail.com>
2026-01-13 13:29:37 +0800

    Added /src/common/hittable_list.h

--- src/common/hittable.h
+++ src/common/hittable.h
@@ -8,6 +8,16 @@ class hit_record {
         point3 p;
         vec3 normal;
         double t;
+        bool font_face;
+
+        void set_face_normal(const ray& r, const vec3& outward_normal) {
+            // Sets the hit record normal vector.
+            // NOTE: the parameter `outward_normal` is assumed to have unit length.
+
+            font_face = dot(r.direction(),outward_normal)<0;
+            normal = font_face ? outward_normal : -outward_normal;
+
+        }
 };
 
 class hittable {
--- src/common/sphere.h
+++ src/common/sphere.h
@@ -28,8 +28,9 @@ class sphere : public hittable {
                 }
 
                 rec.t = root;
-                rec.p = r.at(rec.t);
-                rec.normal = (rec.p - center) / radius;
+                rec.p = r.at(rec.t);   
+                vec3 outward_normal = (rec.p - center) / radius;
+                rec.set_face_normal(r, outward_normal);
                 
                 return true;
         }
