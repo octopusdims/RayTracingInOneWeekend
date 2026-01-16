186159baff1538fc87fde7a850bfa18c845f0283
zhanghanyu <octopusdims@gmail.com>
2026-01-16 18:28:05 +0800

    Implemented Defocus Blur

--- src/common/vec3.h
+++ src/common/vec3.h
@@ -56,27 +56,35 @@ using point3=vec3;
 inline std::ostream& operator<<(std::ostream& out,const vec3& v) {
     return out<<v.e[0]<<' '<<v.e[1]<<' '<<v.e[2];
 }
+
 inline vec3 operator+(const vec3& u, const vec3& v) {
     return vec3(u.e[0]+v.e[0],u.e[1]+v.e[1],u.e[2]+v.e[2]);
 }
+
 inline vec3 operator-(const vec3& u, const vec3& v) {
     return vec3(u.e[0]-v.e[0],u.e[1]-v.e[1],u.e[2]-v.e[2]);
 }
+
 inline vec3 operator*(const vec3& u, const vec3& v) {
     return vec3(u.e[0]*v.e[0],u.e[1]*v.e[1],u.e[2]*v.e[2]);
 }
+
 inline vec3 operator*(double t,const vec3& v) {
     return vec3(t*v.e[0],t*v.e[1],t*v.e[2]);
 }
+
 inline vec3 operator*(const vec3& v, double t) {
     return t*v;
 }
+
 inline vec3 operator/(const vec3& v, double t) {
     return (1/t)*v;
 }
+
 inline double dot(const vec3& u, const vec3& v) {
     return u.e[0]*v.e[0]+u.e[1]*v.e[1]+u.e[2]*v.e[2];
 }
+
 inline vec3 cross(const vec3& u, const vec3& v) {
     return vec3(u.e[1]*v.e[2]-u.e[2]*v.e[1],
                 u.e[2]*v.e[0]-u.e[0]*v.e[2],
@@ -85,6 +93,15 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 inline vec3 unit_vector(const vec3& v) {
     return v/v.length();
 }
+
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
 inline vec3 random_unit_vector() {
     while (true) {
         auto p = vec3::random(-1,1);
@@ -93,6 +110,7 @@ inline vec3 random_unit_vector() {
             return p / sqrt(lensq);
     }
 }
+
 inline vec3 random_on_hemisphere (const vec3& normal) {
     vec3 on_unit_sphere = random_unit_vector();
     if (dot(on_unit_sphere, normal) > 0.0) 
