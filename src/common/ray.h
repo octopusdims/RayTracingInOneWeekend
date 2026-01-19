083665083b54eee7b8a2a4f3c2c9127b55486fbd
zhanghanyu <octopusdims@gmail.com>
2026-01-19 13:58:41 +0800

    Added Motion Blur

--- src/common/ray.h
+++ src/common/ray.h
@@ -7,10 +7,13 @@
 class ray {
     public:
         ray() {}
-        ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+        ray(const point3& origin, const vec3& direction) : ray(origin, direction, 0) {}
+        ray(const point3& origin, const vec3& direction, double time) : orig(origin), dir(direction), tm(time) {}
+        
 
         const point3& origin() const {return orig;}
         const vec3& direction() const {return dir;}
+        double time() const {return tm;}
 
         point3 at (double t) const {
             return orig + t*dir;
@@ -19,5 +22,6 @@ class ray {
     private:
         point3 orig;    //The origin of the ray.
         vec3 dir;       //The direction of ray.
+        double tm;      //Time information
 };
 #endif
